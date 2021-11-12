2afca92d12fe87f43f33a855ccff0b12be5851ab
Luiz <luiz@ares.com.au>
2021-11-12 11:16:02 +1100

    Add project files.

---
+++ DotNetFrameworkApp.API/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DotNetFrameworkApp.API
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DotNetFrameworkApp.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DotNetFrameworkApp.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
